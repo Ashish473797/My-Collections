a1acb6e9d42c5844da0931f598fc112a6ebbd47d
ashish473797 <ashish473797@gmail.com>
2024-10-02 22:58:42 +0530

    updated

--- src/modules/ContactUs.tsx
+++ src/modules/ContactUs.tsx
@@ -56,7 +56,7 @@ export default function ContactUs() {
       </div>
 
       {/* query  */}
-      <div className="container mx-auto my-24 px-4">
+      <div className="container mx-auto my-12 lg:my-24 px-4">
           <h2 className="text-center text-2xl lg:text-3xl font-semibold mb-4">Any other questions?</h2>
           <p className="text-sm lg:text-base text-center font-medium text-gray-700 mb-16">We’re here to help. You can contact us: Weekdays: 6am to 6pm PT, Weekends: 6am to 6pm PT.</p>
           <div className="border border-gray-300 lg:mx-32 rounded-3xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 p-8">

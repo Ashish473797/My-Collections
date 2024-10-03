2cb1c91bd834a9b6b4f83145377b3ce4e79c6005
ashish473797 <ashish473797@gmail.com>
2024-10-03 19:53:53 +0530

    updated

--- src/modules/components/CollectionProducts/CollectionProducts.tsx
+++ src/modules/components/CollectionProducts/CollectionProducts.tsx
@@ -4,11 +4,14 @@ import Button from "../../../components/Button/Button";
 import { AppDispatch, RootState } from "../../../redux/store";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchProducts } from "../../../redux/productsSlice";
+import { fetchProducts, Product } from "../../../redux/productsSlice";
 import { addToCart } from "../../../redux/cartSlice";
 
 export default function CollectionProducts() {
-  const [gridColunm, setGridColunm] = useState(4);
+
+  const [gridColumn, setGridColumn] = useState(4);
+  const [visibleProducts, setVisibleProducts] = useState(12);
+  const [sortOption, setSortOption] = useState<string | undefined>("");
 
   const dispatch: AppDispatch = useDispatch();
 
@@ -16,16 +19,43 @@ export default function CollectionProducts() {
     (state: RootState) => state.productsData.products
   );
 
-  console.log(products);
-
   useEffect(() => {
     dispatch(fetchProducts());
   }, [dispatch]);
 
+  
+  const showMoreProducts = () => {
+    setVisibleProducts((prevVisible) => prevVisible + 4);
+  };
+
+  const handleSort = (products: Product[]) => {
+    const sortedProducts = [...products];
+    switch (sortOption) {
+      case "low-to-high":
+        return sortedProducts.sort((a, b) => a.sellingPrice - b.sellingPrice);
+      case "high-to-low":
+        return sortedProducts.sort((a, b) => b.sellingPrice - a.sellingPrice);
+      case "a-z":
+        return sortedProducts.sort((a, b) =>
+          a.name.localeCompare(b.name, undefined, { sensitivity: "base" })
+        );
+      case "z-a":
+        return sortedProducts.sort((a, b) =>
+          b.name.localeCompare(a.name, undefined, { sensitivity: "base" })
+        );
+      default:
+        return sortedProducts;
+    }
+  };
+
+  const sortedProducts = handleSort(products);
+  const displayedProducts = sortedProducts.slice(0, visibleProducts);
+
   return (
     <div className="container mx-auto px-4 md:px-24 mb-24">
-      {/* filters  */}
+      {/* filters */}
       <div className="grid grid-cols-2 lg:grid-cols-3 justify-between my-12">
+        
         {/* Filter Section */}
         <div className="text-xl flex items-center gap-1 text-black cursor-pointer">
           <HiOutlineAdjustmentsVertical />
@@ -34,169 +64,63 @@ export default function CollectionProducts() {
 
         {/* Grid Selection Section */}
         <div className="hidden lg:flex gap-4 justify-center items-center">
-          <div className="flex gap-3">
-            {/* 2-column grid option */}
-            <div
-              className="flex flex-col items-center group"
-              onClick={() => {
-                setGridColunm(2);
-              }}
-            >
-              <div className="grid grid-cols-2 gap-1 p-2 cursor-pointer">
+          <div
+            className="flex flex-col items-center group"
+            onClick={() => setGridColumn(2)}
+          >
+            <div className="grid grid-cols-2 gap-1 p-2 cursor-pointer">
+              {[...Array(4)].map((_, index) => (
                 <div
+                  key={index}
                   className={`w-2 h-[6px] ${
-                    gridColunm === 2 ? "bg-gray-500" : "bg-gray-400"
+                    gridColumn === 2 ? "bg-gray-500" : "bg-gray-400"
                   } group-hover:bg-gray-500 transition duration-200`}
-                ></div>
-                <div
-                  className={`w-2 h-[6px] ${
-                    gridColunm === 2 ? "bg-gray-500" : "bg-gray-400"
-                  } group-hover:bg-gray-500 transition duration-200`}
-                ></div>
-                <div
-                  className={`w-2 h-[6px] ${
-                    gridColunm === 2 ? "bg-gray-500" : "bg-gray-400"
-                  } group-hover:bg-gray-500 transition duration-200`}
-                ></div>
-                <div
-                  className={`w-2 h-[6px] ${
-                    gridColunm === 2 ? "bg-gray-500" : "bg-gray-400"
-                  } group-hover:bg-gray-500 transition duration-200`}
-                ></div>
-              </div>
+                />
+              ))}
             </div>
+          </div>
 
-            {/* 4-column grid option */}
-            <div
-              className="flex flex-col items-center group"
-              onClick={() => {
-                setGridColunm(4);
-              }}
-            >
-              <div className="grid grid-cols-4 gap-1 p-2 cursor-pointer">
+          <div
+            className="flex flex-col items-center group"
+            onClick={() => setGridColumn(4)}
+          >
+            <div className="grid grid-cols-4 gap-1 p-2 cursor-pointer">
+              {[...Array(8)].map((_, index) => (
                 <div
+                  key={index}
                   className={`w-2 h-[6px] ${
-                    gridColunm === 4 ? "bg-gray-500" : "bg-gray-400"
+                    gridColumn === 4 ? "bg-gray-500" : "bg-gray-400"
                   } group-hover:bg-gray-500 transition duration-200`}
-                ></div>
-                <div
-                  className={`w-2 h-[6px] ${
-                    gridColunm === 4 ? "bg-gray-500" : "bg-gray-400"
-                  } group-hover:bg-gray-500 transition duration-200`}
-                ></div>
-                <div
-                  className={`w-2 h-[6px] ${
-                    gridColunm === 4 ? "bg-gray-500" : "bg-gray-400"
-                  } group-hover:bg-gray-500 transition duration-200`}
-                ></div>
-                <div
-                  className={`w-2 h-[6px] ${
-                    gridColunm === 4 ? "bg-gray-500" : "bg-gray-400"
-                  } group-hover:bg-gray-500 transition duration-200`}
-                ></div>
-                <div
-                  className={`w-2 h-[6px] ${
-                    gridColunm === 4 ? "bg-gray-500" : "bg-gray-400"
-                  } group-hover:bg-gray-500 transition duration-200`}
-                ></div>
-                <div
-                  className={`w-2 h-[6px] ${
-                    gridColunm === 4 ? "bg-gray-500" : "bg-gray-400"
-                  } group-hover:bg-gray-500 transition duration-200`}
-                ></div>
-                <div
-                  className={`w-2 h-[6px] ${
-                    gridColunm === 4 ? "bg-gray-500" : "bg-gray-400"
-                  } group-hover:bg-gray-500 transition duration-200`}
-                ></div>
-                <div
-                  className={`w-2 h-[6px] ${
-                    gridColunm === 4 ? "bg-gray-500" : "bg-gray-400"
-                  } group-hover:bg-gray-500 transition duration-200`}
-                ></div>
-              </div>
+                />
+              ))}
             </div>
+          </div>
 
-            {/* 6-column grid option */}
-            <div
-              className="flex flex-col items-center group"
-              onClick={() => {
-                setGridColunm(6);
-              }}
-            >
-              <div className="grid grid-cols-6 gap-1 p-2 cursor-pointer">
+          <div
+            className="flex flex-col items-center group"
+            onClick={() => setGridColumn(6)}
+          >
+            <div className="grid grid-cols-6 gap-1 p-2 cursor-pointer">
+              {[...Array(12)].map((_, index) => (
                 <div
+                  key={index}
                   className={`w-2 h-[6px] ${
-                    gridColunm === 6 ? "bg-gray-500" : "bg-gray-400"
+                    gridColumn === 6 ? "bg-gray-500" : "bg-gray-400"
                   } group-hover:bg-gray-500 transition duration-200`}
-                ></div>
-                <div
-                  className={`w-2 h-[6px] ${
-                    gridColunm === 6 ? "bg-gray-500" : "bg-gray-400"
-                  } group-hover:bg-gray-500 transition duration-200`}
-                ></div>
-                <div
-                  className={`w-2 h-[6px] ${
-                    gridColunm === 6 ? "bg-gray-500" : "bg-gray-400"
-                  } group-hover:bg-gray-500 transition duration-200`}
-                ></div>
-                <div
-                  className={`w-2 h-[6px] ${
-                    gridColunm === 6 ? "bg-gray-500" : "bg-gray-400"
-                  } group-hover:bg-gray-500 transition duration-200`}
-                ></div>
-                <div
-                  className={`w-2 h-[6px] ${
-                    gridColunm === 6 ? "bg-gray-500" : "bg-gray-400"
-                  } group-hover:bg-gray-500 transition duration-200`}
-                ></div>
-                <div
-                  className={`w-2 h-[6px] ${
-                    gridColunm === 6 ? "bg-gray-500" : "bg-gray-400"
-                  } group-hover:bg-gray-500 transition duration-200`}
-                ></div>
-                <div
-                  className={`w-2 h-[6px] ${
-                    gridColunm === 6 ? "bg-gray-500" : "bg-gray-400"
-                  } group-hover:bg-gray-500 transition duration-200`}
-                ></div>
-                <div
-                  className={`w-2 h-[6px] ${
-                    gridColunm === 6 ? "bg-gray-500" : "bg-gray-400"
-                  } group-hover:bg-gray-500 transition duration-200`}
-                ></div>
-                <div
-                  className={`w-2 h-[6px] ${
-                    gridColunm === 6 ? "bg-gray-500" : "bg-gray-400"
-                  } group-hover:bg-gray-500 transition duration-200`}
-                ></div>
-                <div
-                  className={`w-2 h-[6px] ${
-                    gridColunm === 6 ? "bg-gray-500" : "bg-gray-400"
-                  } group-hover:bg-gray-500 transition duration-200`}
-                ></div>
-                <div
-                  className={`w-2 h-[6px] ${
-                    gridColunm === 6 ? "bg-gray-500" : "bg-gray-400"
-                  } group-hover:bg-gray-500 transition duration-200`}
-                ></div>
-                <div
-                  className={`w-2 h-[6px] ${
-                    gridColunm === 6 ? "bg-gray-500" : "bg-gray-400"
-                  } group-hover:bg-gray-500 transition duration-200`}
-                ></div>
-              </div>
+                />
+              ))}
             </div>
           </div>
         </div>
 
         {/* Sort By Section */}
         <div className="flex justify-end items-center gap-2">
-          {/* <p className="text-base">Sort By</p> */}
-          <select className="select select-bordered select-sm" defaultValue="">
-            <option value="" disabled>
-              Sort By
-            </option>
+          <select
+            className="select select-bordered select-sm"
+            value={sortOption}
+            onChange={(e) => setSortOption(e.target.value)}
+          >
+            <option value="">Sort By</option>
             <option value="low-to-high">Price, Low to High</option>
             <option value="high-to-low">Price, High to Low</option>
             <option value="a-z">Alphabetically, A - Z</option>
@@ -205,11 +129,11 @@ export default function CollectionProducts() {
         </div>
       </div>
 
-      {/* products  */}
+      {/* products */}
       <div
-        className={`grid grid-cols-1 md:grid-cols-2 col-${gridColunm} gap-6`}
+        className={`grid grid-cols-1 md:grid-cols-2 col-${gridColumn} gap-6`}
       >
-        {products.map((product, index) => (
+        {displayedProducts.map((product, index) => (
           <ProductCard
             key={index}
             name={product.name}
@@ -221,11 +145,17 @@ export default function CollectionProducts() {
         ))}
       </div>
 
-      <div className="flex justify-center mt-16">
-        <Button className="text-sm lg:text-base py-2 lg:py-3 px-5 lg:px-7">
-          Show More Products
-        </Button>
-      </div>
+      {/* Show more products button */}
+      {visibleProducts < products.length && (
+        <div className="flex justify-center mt-16">
+          <Button
+            className="text-sm lg:text-base py-2 lg:py-3 px-5 lg:px-7"
+            onClick={showMoreProducts}
+          >
+            Show More Products
+          </Button>
+        </div>
+      )}
     </div>
   );
 }

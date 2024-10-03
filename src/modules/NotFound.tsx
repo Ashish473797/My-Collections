5e7a770f9756b45dec257329778e787d05a2043f
ashish473797 <ashish473797@gmail.com>
2024-10-03 17:13:12 +0530

    updated

--- src/modules/NotFound.tsx
+++ src/modules/NotFound.tsx
@@ -1,8 +1,13 @@
-
 export default function NotFound() {
   return (
-    <div className="h-[85vh] flex justify-center items-center text-xl md:text-4xl">
-        <h1>404 (Page Not Found!)</h1>
+    <div className="h-[85vh] flex flex-col justify-center items-center text-center bg-gray-100">
+      <div className="text-8xl mb-8">😕</div>
+      <h1 className="text-xl md:text-4xl font-bold text-gray-800">
+        404 (Page Not Found!)
+      </h1>
+      <p className="mt-4 text-gray-500">
+        Oops! The page you're looking for doesn't exist.
+      </p>
     </div>
-  )
+  );
 }
